6f6b5ebef5d799eaa9d5e625b6c9f6cad8740b20
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2018-05-31 20:43:38 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  //2. Получить элемент, к которому необходимо добавить маску
+  $("#phone").mask("8(999) 999-9999");
+});
+
+
+$('.rewievs__slider').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+});
